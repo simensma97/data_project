fca5447871038f546de29bb6e4c532bd1740238a
simensma97 <66841346+simensma97@users.noreply.github.com>
2020-10-15 16:58:03 +0200

    Create main.py

---
+++ simen/main.py
@@ -0,0 +1,26 @@
+#import matplotlib.pyplot as plt
+import os
+from data_man import data
+
+path = './chords'
+
+def tune(path):
+  chords_list = os.listdir(path)
+  
+  freq_chords = []
+  name_chords = []
+  for i in range(len(chords_list)):
+    if chords_list[i].endswith('.wav'):
+      a = data(chords_list[i])
+      freq = data.fft(a)[0]
+      name = chords_list[i].split('.')[0]
+    
+      name_chords = np.append(name_chords, name)
+      freq_chords = np.append(freq_chords, freq)
+  return name_chords, freq_chords
+
+
+tuning = tune(path)
+
+
+ 
