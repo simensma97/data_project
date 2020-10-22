008baeb07dfae2bbf5a91f5d3aab98dd546a449c
simensma97 <66841346+simensma97@users.noreply.github.com>
2020-10-22 19:01:44 +0200

    Update data_man.py

--- simen/data_man.py
+++ simen/data_man.py
@@ -1,14 +1,25 @@
 import os
 import numpy as np
 import wave
+from collections import Counter
 
 
 class data:
-    fr = 44100
+
+    array_chords = np.array([
+                         [79.40368652	,105.6472778	,152.0782471	,202.5466919	,257.0526123	,339.8208618],
+                         [86.8057251	,113.0493164	,161.4990234	,214.6591187	,271.8566895	,357.989502],
+                         [91.51611328	,119.7784424	,170.9197998	,228.1173706	,288.0065918	,380.1956177],
+                         [95.55358887	,127.180481	,181.0134888	,241.5756226	,305.5023193	,403.074646],
+                         [100.9368896	,135.2554321	,191.1071777	,256.3796997	,323.6709595	,426.6265869]
+    ])
+
+
+    fr = 22050
     max = 500
 
     def __init__(self, tune):
-      self.dir = './chords'
+      self.dir = '/content'
       self.path = os.path.join(self.dir, tune)
         
       self.list_freq = []
@@ -62,42 +73,63 @@ class data:
           self.start = i
           break
     
-    def get_chords(self):
+    def get_chords(self,nr):
       self.freq_array = self.fft()[1]
       self.mag_array = self.fft()[2]
-      self.sort_mag = np.sort(self.mag_array)[::-1] 
-      #print(self.sort_mag[:10])
+      #self.sort_mag = np.sort(self.mag_array)[::-1]
 
-      
+      self.new_freq = []
+      self.mag_freq = []
 
-      for i in range(len(self.sort_mag)):
-        mag = self.sort_mag[i]
-        ind = np.where(self.mag_array==mag)
+      for i in range(len(self.freq_array)):
+        freq = self.freq_array[i]
+        mag = self.mag_array[i]
+        if freq<np.amax(self.array_chords) or freq>np.amin(self.array_chords):
+          self.new_freq = np.append(self.new_freq, freq)
+          self.mag_freq = np.append(self.mag_freq, mag)
 
-        freq = self.freq_array[ind]
-        self.list_freq = np.append(self.list_freq, freq)
+      self.sort_mag_freq = np.sort(self.mag_freq)[::-1]
 
-      self.list_freq = np.sort(self.list_freq[:20])
+      self.top_freq = []
 
-      #print(self.list_freq)
+      for i in range(100):
+        index = np.where(self.mag_freq == self.sort_mag_freq[i])
+        
+        frequency = self.new_freq[index]
+        self.top_freq = np.append(self.top_freq, int(frequency))
 
+      nr = Counter(self.top_freq)
+      list_common = nr.most_common()
 
-      self.freq_pres = np.append(self.freq_pres, self.list_freq[0])
+      self.array_chords = self.array_chords.astype(int)
 
-      self.start=1
+      #print(self.top_freq)
 
-      for i in range(2):
-        self.find_max(self.freq_pres[i])
+      self.chords = []
+      counter = -1
 
+      for i in range(len(list_common)):
+        common = list_common[i]
+        freq = common[0]
 
-      self.freq_pres = np.sort(self.freq_pres)
+        if freq in self.array_chords:
+          counter +=1
 
-      print(self.freq_pres)
+          index = np.where(self.array_chords == freq)
+          #print(index)
 
-      return self.freq_pres
+          
 
-
-
-a = data('test1.wav')
-b = data.get_chords(a)
+          if index[1] == 0:
+            print("1E", str(index[0]))
+          elif index[1] ==1:
+            print("2A", str(index[0]))
+          elif index[1] ==2:
+            print("3D", str(index[0]+1))
+          elif index[1] ==3:
+            print("4G", str(index[0]+1))
+          elif index[1] ==4:
+            print("5B", str(index[0]+1))
+          elif index[1] ==5:
+            print("6E", str(index[0]+1))
 
